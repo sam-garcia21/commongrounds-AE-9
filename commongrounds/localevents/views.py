ef33b5f5682e5373d0822c0a141c0a9892aa658c
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-05 02:48:00 +0800

    Created views and templates for event list and event detail.

--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from localevents.models import Event
 
 # Create your views here.
+class EventListView(ListView):
+    model = Event
+    template_name = 'event_list.html'
+
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'event_detail.html'
