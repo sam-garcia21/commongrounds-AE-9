267aab5b6cd9ba291a547f105449cde9e23d2004
FaithPalencia <kristin.palencia@student.ateneo.edu>
2026-03-05 02:56:15 +0800

    Added appropriate admin panels for models.

--- commongrounds/localevents/admin.py
+++ commongrounds/localevents/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Event, EventType
 
 # Register your models here.
+class EventTypeAdmin(admin.ModelAdmin):
+    model = EventType
+
+
+class EventAdmin(admin.ModelAdmin):
+    model = Event
+
+
+admin.site.register(EventType, EventTypeAdmin)
+admin.site.register(Event, EventAdmin)
