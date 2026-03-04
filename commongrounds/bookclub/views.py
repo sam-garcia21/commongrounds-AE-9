10c65b44f06244364452b8ea77ba8a7b57369780
aellissia <maria.angelica.munoz@student.ateneo.edu>
2026-03-05 00:26:29 +0800

    Added views and initial template folder for bookclub.

--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Book, Genre
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    ctx = {
+        "books" : books
+    }
+    return render(request, "bookclub/book_list.html", ctx)
+
+def book_detail(request, pk):
+    book = Book,objects.get(id=pk)
+    ctx = {
+        "book" : book
+    }
+    return render(request, "bookclub/book_detail.html", ctx)
