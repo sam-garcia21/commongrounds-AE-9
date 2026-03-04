78a978e608321b6d423638ac3046ce64bef25ce3
aellissia <maria.angelica.munoz@student.ateneo.edu>
2026-03-05 00:43:14 +0800

    Added admin and updated app-level urls for bookclub.

--- commongrounds/bookclub/urls.py
+++ commongrounds/bookclub/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name="bookclub"
 
 urlpatterns = [
-    
+    path('books', views.book_list, name='book_list')
+    path('book/<int:pk>', views.book_detail, name='book_detail')
 ]
