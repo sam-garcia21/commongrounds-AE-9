c9a77d3e7ffabcbfa4bbf4302e80d02938ade5d7
Kayin Miranda <kayin.edianne.miranda@student.ateneo.edu>
2026-03-05 15:51:31 +0800

    made ProjectCategory and Project models

--- commongrounds/diyprojects/models.py
+++ commongrounds/diyprojects/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    class Meta: 
+        ordering = ['name']
+        verbose_name = 'projectcategory'
+        verbose_name_plural = 'project categories'
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ProjectCategory, on_delete=models.SET_NULL, related_name='category', null=True, blank=True)
+    description = models.TextField()
+    materials = models.TextField()
+    steps = models.TextField()
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def get_absolute_url(self):
+        return reverse('diyprojects:diyprojects_detail', args=[str(self.id)])
+    
+    class Meta: 
+        ordering = ['created_on']
+        verbose_name = 'projectcategory'
+        verbose_name_plural = 'project categories'
