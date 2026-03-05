b1d519d5a9510ef77dcfb9beaa957aeb9bdab185
Sam Garcia <samantha.sofia.garcia@student.ateneo.edu>
2026-03-05 16:57:24 +0800

    created admin panels for commission and commissionTypes

--- commongrounds/commissions/admin.py
+++ commongrounds/commissions/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Commission, CommissionType
+
+class CommissionInline(admin.TabularInline):
+    model = Commission
+
+class CommissionTypeAdmin(admin.ModelAdmin):
+    model = CommissionType
+    inlines = [CommissionInline]
+
+class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
+
+    search_fields = ('title',)
+
+    list_display = ('title', 'commission_type',)
+
+    list_filter = ('commission_type', )
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                'title', 'commission_type', 'people_required', 'description'
+            ]
+        })
+    ]
+
+admin.site.register(CommissionType, CommissionTypeAdmin)
+admin.site.register(Commission, CommissionAdmin)
