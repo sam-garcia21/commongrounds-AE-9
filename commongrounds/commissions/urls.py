9aaf3551f073ea38eed19c6fa486df1f6f594028
Sam Garcia <samantha.sofia.garcia@student.ateneo.edu>
2026-03-05 16:55:16 +0800

    fixed formatting

--- commongrounds/commissions/urls.py
+++ commongrounds/commissions/urls.py
@@ -4,7 +4,8 @@ from .views import CommissionListView, CommissionDetailView
 
 urlpatterns = [
     path('requests/', CommissionListView.as_view(), name='commission_list'),
-    path('request/<int:pk>/', CommissionDetailView.as_view(), name='commission_detail'),
+    path('request/<int:pk>/', CommissionDetailView.as_view(),
+         name='commission_detail'),
 ]
 
 app_name = 'commissions'
