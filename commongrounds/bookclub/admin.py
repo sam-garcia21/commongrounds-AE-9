fadf98f3419a0449bedf51b5d2e3eac3b86f521e
aellissia <maria.angelica.munoz@student.ateneo.edu>
2026-05-03 23:40:32 +0800

    Added admin.

--- commongrounds/bookclub/admin.py
+++ commongrounds/bookclub/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Genre, Book
+from .models import Genre, Book, Profile, Bookmark, Review, Borrow
 
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'display_name', 'role',)
+    search_fields = ('user__username', 'displaye_name',)
+    list_filter = ('role',)
 
 class GenreAdmin(admin.ModelAdmin):
     model = Genre 
@@ -14,5 +19,20 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'genre', 'publication_year', )
     list_filter = ('author', 'genre', 'publication_year', )
 
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('book', 'profile',)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('book', 'title', 'user_reviewer', 'anon_reviewer',)
+    list_filter = ('book',)
+
+class BorrowAdmin(admin.ModelAdmin):
+    list_display = ('book', 'name', 'date_borrowed', 'date_return',)
+    readonly_fields = ('date_return',)
+
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Book, BookAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Borrow, BorrowAdmin)
