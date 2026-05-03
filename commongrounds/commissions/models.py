4b5977f3226891775a55b02fffb4a2bae85e6ce0
Sam Garcia <samantha.sofia.garcia@student.ateneo.edu>
2026-05-03 17:24:49 +0800

    updated commission model

--- commongrounds/commissions/models.py
+++ commongrounds/commissions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+from accounts.models import Profile
 
 class CommissionType(models.Model):
     name = models.CharField(max_length=255)
@@ -14,6 +15,12 @@ class CommissionType(models.Model):
 
 
 class Commission(models.Model):
+    OPEN = 'open'
+    FULL = 'full'
+    STATUS_CHOICES = {
+        OPEN: "Open",
+        FULL: "Full",
+    }
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     commission_type = models.ForeignKey(
@@ -22,7 +29,17 @@ class Commission(models.Model):
         related_name='commissions',
         null=True
     )
+    maker = models.ForeignKey(
+        Profile, 
+        on_delete=models.CASCADE,
+        related_name='commisions',
+    )
     people_required = models.IntegerField()
+    status = models.CharField(
+        max_length=4,
+        choices=STATUS_CHOICES,
+        default=OPEN
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
