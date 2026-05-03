614385247bbfd7bea578f8df8125aca15962a7ef
aellissia <maria.angelica.munoz@student.ateneo.edu>
2026-05-03 21:46:17 +0800

    Added views and edited urls.

--- commongrounds/bookclub/urls.py
+++ commongrounds/bookclub/urls.py
@@ -6,4 +6,7 @@ app_name="bookclub"
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('book/<int:pk>', views.book_detail, name='book_detail'),
+    path('book/add', views.book_create, name='book_create'),
+    path('book/<int:pk>/edit/', views.book_update, name='book_update'),
+    path('book/<int:pk>/borrow/', views.book_borrow, name='book_borrow'),
 ]
--- commongrounds/bookclub/views.py
+++ commongrounds/bookclub/views.py
@@ -1,17 +1,48 @@
 from django.shortcuts import render
-from .models import Book, Genre
+from .models import Book, Genre, BookReview, Bookmark, Borrow
+from datetime import date
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()
-    ctx = {
-        "books" : books
-    }
-    return render(request, "book_list.html", ctx)
+    contributed = []
+    bookmarked = []
+    reviewed = []
+    if request.user.is_authenticated:
+        profile = request.user.profile
+        contributed = Book.objects.filter(contributor=profile)
+        bookmarked = Books.objects.filter(bookmarks__profile=profile)
+        reviewed = Books.objects.filter(reviews__user_reviewer=profile)
+
+        books = books.exclude(contributor=profile)
+        books = books.exclude(bookmarks__profile=profile)
+        books = books.exclude(reviews__user_reviewer=profile)
+    return render(request, "book_list.html", {
+        "books" : books,
+        "contributed" : contributed,
+        "bookmarked" : bookmarked,
+        "reviewed" : reviewed,
+    })
 
 def book_detail(request, pk):
-    book = Book.objects.get(id=pk)
-    ctx = {
-        "book" : book
-    }
-    return render(request, "book_detail.html", ctx)
+    book = get_object_or_404(Book, id=pk)
+    bookmarks_count = book.bookmars.count()
+    reviews = book.reviews.all()
+
+    can_edit = False 
+    if request.user.is_authenticated and book.contributor == request.user.profile:
+        can_edit = True
+    return render(request, "book_detail.html", {
+        "book" : book,
+        "bookmarks_count" : bookmarks_count
+        "reviews" : reviews,
+        "can_edit" : can_edit
+    })
+
+@login_required
+def book_create(request):
+    if request.user.profile.role != "Book Contributor":
+        return render(request, "403.html")
+
+    return render(request, "book_form.html")
