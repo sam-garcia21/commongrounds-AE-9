0dd2db5eb873bfb2e9c32b36c2b7fdd44d8bfbb6
SIU-914 <ian.uy@student.ateneo.edu>
2026-05-04 20:07:42 +0800

    Fixed login/logout problem

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-LOGIN_REDIRECT_URL = '/merchstore/items'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGIN_URL = '/accounts/login/'
