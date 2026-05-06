8de21c2a83c3cb9ecff8d35e41d0f16ed85efee5
Sam Garcia <samantha.sofia.garcia@student.ateneo.edu>
2026-05-06 18:24:24 +0800

    changed static_root

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -145,7 +145,7 @@ MEDIA_ROOT = BASE_DIR/'media'
 MEDIA_URL = '/media/'
 
 STATIC_URL = 'static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
